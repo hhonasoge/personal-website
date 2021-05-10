3467071d2214989b6566e67681e5c0a9e6130279
Harsha Honasoge <harsha.honasoge@gmail.com>
2021-05-10 15:28:03 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path")
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.get('*', (req, res)=>{
+  console.log("getting here");
+  return res.sendFile(path.join(__dirname+"/client/build/index.html"))
+})
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
