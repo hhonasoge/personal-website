f5cdad78b2b12b5a28d19fe725d2e04f753a97f8
Harsha Honasoge <harsha.honasoge@gmail.com>
2021-05-18 21:29:57 -0500

    update home and about me for small screen

--- client/src/SocialIcons.js
+++ client/src/SocialIcons.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function SocialIcons(props){
   return (
-      <footer className="flex justify-around mt-2 mx-2">
+      <footer className="flex justify-around mx-20 md:mt-2 md:mx-2">
         <a href="https://www.facebook.com/harsha.honasoge" className="fa fa-facebook text-base md:text-xl px-1"></a>
         <a href="https://github.com/hhonasoge" className="fa fa-github text-base md:text-xl px-1"></a>
         <a href="https://www.linkedin.com/in/hhonasoge/" className="fa fa-linkedin text-base md:text-xl px-1"></a>
--- client/src/content.js
+++ client/src/content.js
@@ -10,13 +10,13 @@ function Content(props){
 
 
   return (
-      <div className="flex-row grid grid-cols-5">
-          <div className="ml-4 flex-col text-blue-200 font-roboto">
-            <img src={process.env.PUBLIC_URL + '/prof_pic.jpg'} className="p-4 rounded-full" alt="prof_pic"/>
+      <div className="flex-row md:flex-row md:grid md:grid-cols-5">
+          <div className="md:ml-4 md:flex-col text-blue-200 font-roboto">
+            <img src={process.env.PUBLIC_URL + '/prof_pic.jpg'} className="h-24 w-24 m-auto md:h-auto md:w-auto md:p-4 rounded-full" alt="prof_pic"/>
             <h4 className="font-roboto-slab p-1 text-center text-base md:text-xl">Harsha Honasoge</h4>
             <SocialIcons/>
            </div>
-          <div className="text-blue-200 font-roboto pt-10 pl-20 md:pr-0 pr-4 md:col-span-3 col-span-4">
+          <div className="text-blue-200 font-roboto pt-10 text-center md:pr-0 md:pl-20 md:pr-4 md:col-span-3">
           <p style={{whiteSpace: 'pre-line'}} className="text-lg">{props.path === "about" ? aboutMeStr : homeStr}</p>
           </div>
       </div>
