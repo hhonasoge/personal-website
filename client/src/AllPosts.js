2a7580ce5f2fe6f452f3bbc13abbb1e8643c32af
Harsha Honasoge <harsha.honasoge@gmail.com>
2022-03-12 15:09:04 -0600

    update formatting

--- client/src/AllPosts.js
+++ client/src/AllPosts.js
@@ -1,22 +1,21 @@
 import React from "react";
-import {useState} from "react"
-import BlogCard from "./BlogCard.js"
-
+import { useState } from "react";
+import BlogCard from "./BlogCard.js";
 
 function AllPosts(props) {
-
-  const [data, setData] = useState()
+  const [data, setData] = useState();
 
   React.useEffect(() => {
     fetch("/api/blog")
-    .then((res) => res.json())
-    .then((data) => setData(data.message))
-    .catch(console.error);
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+      .catch(console.error);
   }, []);
 
   return (
-      <div>
-        {(data) ? data.map((post) => (
+    <div>
+      {data ? (
+        data.map((post) => (
           <div key={post._id} className="cards m-10 md:m-auto">
             <BlogCard
               key={post._id}
@@ -25,12 +24,15 @@ function AllPosts(props) {
               title={post.title}
               description={post.description}
               createdAt={new Date(post.createdAt).toLocaleDateString()}
-              body={post.body} />
-            </div>
-        )) : <h3 className="m-10 md:m-auto">Loading blog posts...</h3>}
-      </div>
-    )
+              body={post.body}
+            />
+          </div>
+        ))
+      ) : (
+        <h3 className="m-10 md:m-auto">Loading blog posts...</h3>
+      )}
+    </div>
+  );
 }
-
 
 export default AllPosts;
